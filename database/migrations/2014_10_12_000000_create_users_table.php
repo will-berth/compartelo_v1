62071d6ed29c168f630fa1d4a69063c5a4cdbddb
Miguel López <winalllpz@gmail.com>
2022-06-08 14:10:17 -0500

    - modificacion de la migracion y modelo user
    Signed-off-by: Miguel López <winalllpz@gmail.com>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,32 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombres', 50)->nullable(false);
+            $table->string('app', 50)->nullable(false);
+            $table->string('apm', 50)->nullable(false);
+            $table->date('f_nacimiento')->nullable(false);
+            $table->string('sexo', 9)->nullable(false);
+            $table->char('telefono', 13)->nullable(false);
+            $table->string('nom_user')->nullable(false)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('foto_per', 100)->nullable(true);
+            $table->string('ine_frontal', 100)->nullable(false);
+            $table->string('ine_reverso', 100)->nullable(false);
+            $table->string('comprobante', 100)->nullable(false);
+            $table->boolean('email_verif')->nullable(false);
+            $table->boolean('estatus')->nullable(false);
+            $table->float('saldo')->nullable(false);
+            $table->string('ciudad', 50)->nullable(false);
+            $table->string('estado', 50)->nullable(false);
+            $table->string('municipio', 50)->nullable(false);
+            $table->string('cp', 5)->nullable(false);
+            $table->string('colonia', 50)->nullable(false);
+            $table->string('calle', 50)->nullable(false);
+            $table->string('n_exterior', 10)->nullable(false);
+            $table->string('n_inteior', 10)->nullable(true);
+            $table->string('referencia', 200)->nullable(false);
             $table->rememberToken();
             $table->timestamps();
         });
