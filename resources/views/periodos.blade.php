213eb6c1b252f4d637a905c475e75e0789893686
SantiagoAngelDiaz <santosangel1601@hotmail.com>
2022-07-09 12:30:33 -0500

    - creacion de modal de periodos
    Signed-off-by: SantiagoAngelDiaz <santosangel1601@hotmail.com>

--- resources/views/periodos.blade.php
+++ resources/views/periodos.blade.php
@@ -8,21 +8,77 @@
     </ol>
 </div>
 <br>
+<div class="row">
+    <div class="col-md-6">
+        <label class="sr-only" for="inlineFormInputGroup">Buscar...</label>
+        <div class="input-group mb-2">
+            <input type="search" class="form-control" id="buscar" placeholder="Buscar..." autocomplete="off">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="icofont-search"></i></div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="d-flex justify-content-end">
+            <button type="button" class="btn btn-primary" onclick="openClose('add-periodos', 'periodos', '', 0)">Agregar periodo</button>
+        </div>
+    </div>
+</div>
+<br>
+
 <div class="d-flex justify-content-end">
     <button type="button" class="btn btn-primary">Agregar</button>
 </div>
 <br>
 <div class="table-reaponsive">
     <table class="table">
-        <thead>
+        <thead
             <tr>
                 <th>Tipo</th>
                 <th>Acciones</th>
             </tr>
         </thead>
         <tbody>
+        <tr>
+                <td colspan="2">
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
+                            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+                            <center><b class="h6">Cargando...</b></center>
+                        </div>
+                    </div>
+                </td>
+            </tr>
 
         </tbody>
     </table>
+</div>
+
+<!-- modal para agregar marcas -->
+<div class="modal fade" id="add-periodos" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="titulo-modal"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="">
+            <div class="row">
+                <div class="col-md-12">
+                    <label for="">periodos</label>
+                    <input type="text" class="form-control" name="periodo" id="periodo" placeholder="Periodo" required autocomplete="off">
+                </div>
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-primary" id="btn-save"></button>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
